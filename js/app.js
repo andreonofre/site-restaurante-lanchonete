cc997440d121b2efaca61864e8aca61ca4e5e8ac
André Onofre <56845035+andreonofre@users.noreply.github.com>
2022-12-30 21:33:05 -0300

    quase lá

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ const loadProducts = (produtos, idDivParent) => {
                 <h4>${produto.title}</h4>
                 <h4>${produto.value}</h4>
                 <p>${produto.description}</p>
-                <button type="button" onclick="modalTrigger()">Quero este prato</button>
+                <button type="button" onclick="modalTrigger(${produto.id})">Quero este prato</button>
             </article>
             `
 
@@ -18,8 +18,16 @@ const loadProducts = (produtos, idDivParent) => {
     });
 };
 
+
 const modalTrigger = (productId) => {
     const modal = document.querySelector(".modal");
+
+    if (productId != null) {
+        const produto = produtos.filter(produto => produto.id == productId)[0];
+        if ( produto != null) {
+            modal.querySelector("#title").value = produto.title;
+        }
+    }
 
     if (modal.classList.contains("hide")) {
         modal.classList.remove("hide");
@@ -28,8 +36,29 @@ const modalTrigger = (productId) => {
     }
 };
 
-loadProducts(produtos, "#product-div")
 
+const whatsappLinkGeneration = (phoneNumber, productTitle, productQuantity, buyerName, buyerAddress, buyerPayment) => `https://api.whatsapp.com/send?phone=${phoneNumber}=&text=Olá, Eu quero: ${productQuantity} ${productTitle} - Entregar para, ${buyerName} no endereço: ${buyerAddress} - a forma de pagamento será: ${buyerPayment}`
+
+
+const checkout = (phoneNumber) => {
+    const form = document.querySelector("#form-product");
+    form.addEventListener("submit", e => {
+        e.preventDefault()
+
+        const productTitle = form.querySelector("input#title").value
+        const productQuantity = form.querySelector("input#quantity").value
+        const buyerName = form.querySelector("input#name").value
+        const buyerAddress = form.querySelector("input#address").value
+        const buyerPayment = form.querySelector("select#payment").value
+     
+        const whatsappUrl = whatsappLinkGeneration(phoneNumber, productTitle, productQuantity, buyerName, buyerAddress, buyerPayment);
+    
+        window.location.href = whatsappUrl
+    });
+}
+
+loadProducts(produtos, "#product-div")
+checkout("5575999536167")
 
 
 
